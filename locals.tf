78e2c3bcaafe3e7ad9281be1e94c8eb448a61c5d
Tarunteja <taruntejamahanthi@gmail.com>
2026-03-20 19:45:01 +0530

    remove provider in module

--- locals.tf
+++ locals.tf
@@ -12,6 +12,5 @@ locals{
     port_no = var.component == "frontend" ? 80 : 8080
     health_check_path = var.component == "frontend" ? "/" : "/health"
     listener_arn = var.component == "frontend" ? aws_ssm_parameter.frontend_listener_arn.value : aws_ssm_parameter.backend_listener_arn.value
-    host_header = var.component == "frontend" ? ["${var.component}.${local.project}-${local.environment}-frontend-alb.${var.domain_name}"] 
-                              : ["${var.component}.${local.project}-${local.environment}-backend-alb.${var.domain_name}"]
+    host_header = var.component == "frontend" ? ["${var.component}.${local.project}-${local.environment}-frontend-alb.${var.domain_name}"] : ["${var.component}.${local.project}-${local.environment}-backend-alb.${var.domain_name}"]
 }
