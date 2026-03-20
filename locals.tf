a293d85c5611958ac829c2e44cb2378f8baa8345
Tarunteja <taruntejamahanthi@gmail.com>
2026-03-20 19:53:35 +0530

    changes to local

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
 locals{
     ami_id = data.aws_ami.joindevops.id
     project = "roboshop"
-    environemt = "dev"
+    environment = "dev"
     common_tags = {
         project = "roboshop"
         env = "dev"
@@ -11,6 +11,6 @@ locals{
     sg_id = data.aws_ssm_parameter.sg_id.value
     port_no = var.component == "frontend" ? 80 : 8080
     health_check_path = var.component == "frontend" ? "/" : "/health"
-    listener_arn = var.component == "frontend" ? aws_ssm_parameter.frontend_listener_arn.value : aws_ssm_parameter.backend_listener_arn.value
+    listener_arn = var.component == "frontend" ? data.aws_ssm_parameter.frontend_listener_arn.value : data.aws_ssm_parameter.backend_listener_arn.value
     host_header = var.component == "frontend" ? ["${var.component}.${local.project}-${local.environment}-frontend-alb.${var.domain_name}"] : ["${var.component}.${local.project}-${local.environment}-backend-alb.${var.domain_name}"]
 }
